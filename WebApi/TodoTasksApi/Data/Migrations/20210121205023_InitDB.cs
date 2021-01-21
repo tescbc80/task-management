1499f576582d0dfedfea8aa82d9e2d09b56cecf2
Shadmehr <y.shadmehr@gmail.com>
2021-01-21 15:57:25 -0500

    Update DB

--- WebApi/TodoTasksApi/Data/Migrations/20210121205023_InitDB.cs
+++ WebApi/TodoTasksApi/Data/Migrations/20210121205023_InitDB.cs
@@ -1,6 +1,6 @@
 ﻿using Microsoft.EntityFrameworkCore.Migrations;
 
-namespace TaskManagement.WebApi.Data.Migrations
+namespace TaskManagement.WebApi.TodoTasksApi.Data.Migrations
 {
     public partial class InitDB : Migration
     {
@@ -12,7 +12,7 @@ namespace TaskManagement.WebApi.Data.Migrations
                 {
                     Id = table.Column<int>(type: "int", nullable: false)
                         .Annotation("SqlServer:Identity", "1, 1"),
-                    Name = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false)
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: true)
                 },
                 constraints: table =>
                 {
@@ -25,35 +25,23 @@ namespace TaskManagement.WebApi.Data.Migrations
                 {
                     Id = table.Column<int>(type: "int", nullable: false)
                         .Annotation("SqlServer:Identity", "1, 1"),
-                    Title = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
-                    Description = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: true),
-                    TaskStatus = table.Column<int>(type: "int", nullable: false),
-                    CategoryId = table.Column<int>(type: "int", nullable: true)
+                    Title = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Status = table.Column<int>(type: "int", nullable: false)
                 },
                 constraints: table =>
                 {
                     table.PrimaryKey("PK_TodoTasks", x => x.Id);
-                    table.ForeignKey(
-                        name: "FK_TodoTasks_Categories_CategoryId",
-                        column: x => x.CategoryId,
-                        principalTable: "Categories",
-                        principalColumn: "Id",
-                        onDelete: ReferentialAction.Restrict);
                 });
-
-            migrationBuilder.CreateIndex(
-                name: "IX_TodoTasks_CategoryId",
-                table: "TodoTasks",
-                column: "CategoryId");
         }
 
         protected override void Down(MigrationBuilder migrationBuilder)
         {
             migrationBuilder.DropTable(
-                name: "TodoTasks");
+                name: "Categories");
 
             migrationBuilder.DropTable(
-                name: "Categories");
+                name: "TodoTasks");
         }
     }
 }
