cf86b825994b97b81b6c42c2f5b8c0e7670c16f1
Y. Shadmehr <y.shadmehr@gmail.com>
2021-01-21 03:06:45 -0500

    Added ef migrations

---
+++ WebAPI/Data/Migrations/20210121080447_DbInitial.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace TaskManagement.WebAPI.Data.Migrations
+{
+    public partial class DbInitial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "TodoTasks",
+                columns: table => new
+                {
+                    TodoTaskId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Title = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TodoTasks", x => x.TodoTaskId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "TodoTasks");
+        }
+    }
+}
