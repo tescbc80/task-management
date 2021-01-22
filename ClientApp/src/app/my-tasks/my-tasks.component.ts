dcbbc9295a774b5a49c851b8a37916ab4bed69fb
Shadmehr <y.shadmehr@gmail.com>
2021-01-21 20:53:46 -0500

    Added getStatusName

--- ClientApp/src/app/my-tasks/my-tasks.component.ts
+++ ClientApp/src/app/my-tasks/my-tasks.component.ts
@@ -8,6 +8,17 @@ import { HttpClient } from '@angular/common/http';
 export class MyTasksComponent {
   public todotasks: TodoTask[];
 
+  getStatusName(task: TodoTask) {
+    switch (task.status) {
+
+      case 2: return "Done";
+      case 1: return "InProgress";
+      case 0: return "Todo";
+
+      default: return "Todo";
+    }
+  }
+
   constructor(http: HttpClient, @Inject('BASE_URL') baseUrl: string) {
     http.get<TodoTask[]>(baseUrl + 'todotask').subscribe(result => {
       this.todotasks = result;
