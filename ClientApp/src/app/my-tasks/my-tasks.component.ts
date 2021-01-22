351939b2e3962430003800dfc9467f3b14f667c7
Shadmehr <y.shadmehr@gmail.com>
2021-01-21 21:33:57 -0500

    Added add button

--- ClientApp/src/app/my-tasks/my-tasks.component.ts
+++ ClientApp/src/app/my-tasks/my-tasks.component.ts
@@ -28,9 +28,28 @@ export class MyTasksComponent {
         this.todotasks.splice(index, 1);
       }, error => console.error(error));
     }
-
   }
 
+  addTask() {
+    var index = this.todotasks.length;
+    let newItem = {} as TodoTask;
+    newItem.title = "Title " + index;
+    newItem.description = "Description " + index;
+
+    const url = `${this.baseUrl}todotask`;
+    return this.http.post(url, newItem).subscribe(result => {
+      this.todotasks.push(result as TodoTask);
+    }, error => console.error(error));
+  }
+
+  updateTask(task: TodoTask, index: number) {
+    if (index > -1) {
+      const url = `${this.baseUrl}todotask/${task.id}`;
+      return this.http.delete(url).subscribe(result => {
+        this.todotasks.splice(index, 1);
+      }, error => console.error(error));
+    }
+  }
   constructor(http: HttpClient, @Inject('BASE_URL') baseUrl: string) {
     this.http = http;
     this.baseUrl = baseUrl;
